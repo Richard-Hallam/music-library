ae9e57582ef0287c2e04e7744c563247addad848
Richard Hallam <richardhallam25@gmail.com>
2023-10-29 17:57:22 +0000

    updated tests

--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,5 +1,6 @@
 const {expect} = require('chai');
 const request = require ('supertest');
+const db = require('../src/db')
 const app = require('../src/app');
 
 describe('create artist',() => {
@@ -12,7 +13,15 @@ describe('create artist',() => {
                 });
 
                 expect(res.status).to.equal(201);
+                expect(body.name).to.equal('Tame Impala');
+                expect(body.genre).to.equal('Rock');
+
+                const {
+                    rows: [artistData],
+                } = await db.query(`SELECT * FROM Artists WHERE id = ${body.id}`);
+                expect(artistData.name).to.equal('Tame Impala');
+                expect(artistData.genre).to.equal('Rock')
+                }
             });
         });
     });
-});
